ede400dfe0ff11b47b83174241e506c5dbd020d7
Radovan Tkac <radovan.tkac@zentity.com>
2021-12-05 00:50:59 +0100

    chore: added husky with lint staged

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { AuthService } from 'src/app/services/auth.service';
   styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent implements OnInit {
-  isLoggedIn: boolean = false;
+  isLoggedIn = false;
 
   constructor(private authService: AuthService) {}
 
